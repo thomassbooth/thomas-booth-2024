1d7dbc66ed8da0a1e50dfb706d045b36707ad3ea
Thomas <thomasbooth26@gmail.com>
2024-01-30 17:11:13 +0000

    update

---
+++ src/components/Cursor/anim.ts
@@ -0,0 +1,4 @@
+export const content = {
+  random: { scale: 1, opacity: 1 },
+  none: { scale: 0, opacity: 0 },
+};
--- src/components/Random.tsx
+++ src/components/Random.tsx
@@ -2,22 +2,30 @@
 import useHover from "@/store/useCursor";
 import React from "react";
 import Card from "./Card";
+import Project from "./Hover/Project";
+import { useRouter } from "next/navigation";
 
 const Random = () => {
-  const [cursor, setCursor] = useHover((state) => [state.cursor, state.setCursor]);
+  const [cursor, setCursor] = useHover((state) => [
+    state.cursor,
+    state.setCursor,
+  ]);
+  const router = useRouter();
   return (
     <div
+      onClick={() => {
+        setCursor({size: 16, type: 'none'})
+        router.push("test");
+      }}
       className="bg-red-300 w-[1000px] h-[200px] text-white z-20"
       onMouseLeave={() => {
-        console.log("leaving");
-        setCursor({size: 16, type: 'none'});
+        setCursor({ size: 16, type: "none" });
       }}
       onMouseEnter={() => {
-        console.log('entering')
-        setCursor({size: 100, type: 'random', content: <Card colour="red"/>});
+        setCursor({ size: 100, type: "random", content: <Project /> });
       }}
     >
-      {cursor.type === 'random' ? "true" : "false"}
+      {cursor.type === "random" ? "true" : "false"}
     </div>
   );
 };

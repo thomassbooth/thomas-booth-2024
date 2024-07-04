a0732df25320dbcddacef48d67896207a886c167
Thomas <thomasbooth26@gmail.com>
2024-07-05 00:26:19 +0400

    update to about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { AiOutlineGlobal } from "react-icons/ai";
+import { FaArrowRight } from "react-icons/fa";
 
 const About = () => {
   const [dimensions, setDimensions] = useState({
@@ -36,11 +37,16 @@ const About = () => {
 
   const titleY = useTransform(scrollYProgress, [0, 1], ["0vh", "-10vh"]);
 
-  const imageY = useTransform(scrollYProgress, [0, 1], ["10vh", "-20vh"]);
+  const imageY = useTransform(scrollYProgress, [0, 1], ["0vh", "-25vh"]);
   const imageWrapperY = useTransform(scrollYProgress, [0, 1], ["0vh", "10vh"]);
   const imageTextY = useTransform(scrollYProgress, [0.2, 1], ["0vh", "50vh"]);
 
   const ballX = useTransform(scrollYProgress, [0, 0.7], ["50vw", "40vw"]);
+  const rotateArrow = useTransform(
+    scrollYProgress,
+    [0.25, 0.7],
+    ["0deg", "90deg"]
+  );
   const rotateIcon = useTransform(
     scrollYProgress,
     [0, 0.7],
@@ -50,9 +56,12 @@ const About = () => {
   return (
     <div
       ref={container}
-      className="bg-common-cream text-common-gray px-[7%] py-[10%]"
+      className="bg-common-cream text-common-gray px-[7%] pt-[10%] pb-[15%]"
     >
-      <motion.h2 style = {{y: titleY}} className="flex flex-col text-subtitle font-semibold pb-[15%] px-[10%]">
+      <motion.h2
+        style={{ y: titleY }}
+        className="flex flex-col text-subtitle font-semibold pb-[15%] px-[10%]"
+      >
         <span>Technical prowess</span>
         <span>to put you ahead</span>
         <div className="mt-[15%] flex items-center w-full">
@@ -70,9 +79,18 @@ const About = () => {
       </motion.h2>
 
       <motion.div className="flex w-full justify-between">
-        <motion.div style = {{y: imageTextY}}>
-          <span>WAG1 BRUV</span>
-        </motion.div>
+        <div className="flex w-1/2">
+          <motion.div style = {{y: imageWrapperY, rotate: rotateArrow}} className = 'absolute'>
+            <FaArrowRight size={50} />
+          </motion.div>
+          <motion.p style={{ y: imageTextY }} className = 'px-20 text-lg font-light'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus
+            non enim praesent elementum facilisis leo. Gravida rutrum quisque
+            non tellus. Quis vel eros donec ac odio tempor orci. Ullamcorper sit
+            amet risus nullam eget.
+          </motion.p>
+        </div>
         <motion.div
           style={{ y: imageWrapperY }}
           className="overflow-hidden h-[70vw]"
